0e4e91d33e8c6ddf94618c70595aa812dd1b58fd
Trent Robbins <robbintt@gmail.com>
2019-06-15 16:18:50 -0700

    Added hibernate feature and turned it off until I am using AMIs instead of ubuntu.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 
 Also might have a function to stop the instance when it receives a signal... and maybe even poll for that signal? Would rather have the signal pushing go to this from the EC2 instance since the EC2 instance is running and the lamdba costs money to run.
+
+
+## Hibernation
+
+Instance hibernation will significantly speed up launch times and preserve existing game state.
+
+Just set `Hibernate=True` in stop_instances.
+
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 ''' Flask service to provide webpage
 
 consider async await with flask -- no idea about support...
+
+    # can dispatch a an email with SNS if it's running for more than a day
+    # would still be nice to get a user count somehow before stopping
+    # the instance does tell the minecraft client user count so maybe it has an api...
+    # the api would have to be at the game port though.... ... hmmm...
 '''
 import json
 from flask import Flask, jsonify
 import boto3
 import aws_secrets
+from datetime import date, datetime
 
 app = Flask(__name__)
 
@@ -17,21 +23,45 @@ client = boto3.client(
 
 minecraft_instance_ids = [aws_secrets.instance_id]
 
-
-# common solution from: https://stackoverflow.com/a/22238613
-from datetime import date, datetime
 def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
-
+    '''JSON serializer for objects not serializable by default json code
+    common solution from: https://stackoverflow.com/a/22238613
+    '''
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
     raise TypeError ("Type %s not serializable" % type(obj))
 
 
-def start_ec2_instance(instance_ids, addl_info='', dry_run=True):
-    ''' Start the ec2 instance
+def stop_ec2_instance(instance_ids, dry_run, hibernate):
+    ''' Hibernate a given instance unless explicitly stopped.
 
-    Consider polling the instance to see its state.
+    Only AMIs support hibernation... the current machine is ubuntu.
+
+    I was planning to switch to AMI, but haven't really messed with it.
+
+    Apparently after 60 days you need to hard stop and hard start it.
+    I don't know how that works, but eh... something to read up on.
+    '''
+    stop_response = client.stop_instances(
+            InstanceIds=instance_ids,
+            Hibernate=hibernate,
+            DryRun=dry_run
+            )
+
+    return stop_response 
+
+def describe_ec2_instance(instance_ids, dry_run):
+    ''' Describe an ec2 instance
+    '''
+    describe_response = client.describe_instances(
+            InstanceIds=instance_ids,
+            DryRun=dry_run
+            )
+
+    return describe_response
+
+def start_ec2_instance(instance_ids, dry_run, addl_info=''):
+    ''' Start the ec2 instance
 
     documentation: 
         - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.start_instances
@@ -43,22 +73,7 @@ def start_ec2_instance(instance_ids, addl_info='', dry_run=True):
             DryRun=dry_run
             )
 
-    # might move this to a separate section of the route
-    # especially if i call describe before start and use a condition to start
-    # i could also include a stop function since that is faster
-    #   removes a roadblock
-    # can dispatch a an email with SNS if it's running for more than a day
-    # would still be nice to get a user count somehow before stopping
-    # the instance does tell the minecraft client user count so maybe it has an api...
-    # the api would have to be at the game port though.... ... hmmm...
-    describe_response = client.describe_instances(
-            InstanceIds=instance_ids,
-            DryRun=dry_run
-            )
-
-    response = [start_response, describe_response]
-
-    return response
+    return start_response
 
 
 @app.route('/start/', methods=["GET", "POST"])
@@ -70,11 +85,31 @@ def start_webpage():
     i could just use the endpoint as validation, e.g. put a UUID in the route
 
     then i could even rotate UUIDs if there's an issue
-    '''
-    
-    response = start_ec2_instance(minecraft_instance_ids, dry_run=False)
 
-    return json.dumps(response, default=json_serial)
+    Consider polling the instance to see its state.
+
+    call describe before start and use a condition to start or stop?
+    '''
+    describe_response = describe_ec2_instance(minecraft_instance_ids, dry_run=False)
+    start_response = start_ec2_instance(minecraft_instance_ids, dry_run=False)
+
+    return json.dumps([start_response, describe_response], default=json_serial)
+
+
+@app.route('/stop/', methods=["GET", "POST"])
+def stop_webpage():
+    ''' start the ec2 instance on page load
+    this probably needs some sort of authentication
+    otherwise any bots that hit the URL will load it
+
+    i could just use the endpoint as validation, e.g. put a UUID in the route
+
+    then i could even rotate UUIDs if there's an issue
+    '''
+    describe_response = describe_ec2_instance(minecraft_instance_ids, dry_run=False)
+    stop_response = stop_ec2_instance(minecraft_instance_ids, dry_run=False, hibernate=False)
+
+    return json.dumps([stop_response, describe_response], default=json_serial)
 
 
 if __name__ == '__main__':
