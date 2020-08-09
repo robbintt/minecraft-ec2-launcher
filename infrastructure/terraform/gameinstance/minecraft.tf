b89f9e351ca40849f2ae558c0f3195c7434f52f0
Trent Robbins <robbintt@gmail.com>
2020-08-08 21:48:01 -0700

    Update packer readme, add new AMI to both launch templates, add report_user_changes to crontab.

--- infrastructure/terraform/gameinstance/minecraft.tf
+++ infrastructure/terraform/gameinstance/minecraft.tf
@@ -108,7 +108,7 @@ resource "aws_key_pair" "minecraft_ec2_key" {
 resource "aws_launch_template" "minecraft_ec2_launcher" {
   tags                                 = merge(local.tf_global_tags, local.project_name_tag)
   name                                 = "minecraft_ec2_launcher"
-  image_id                             = "ami-0262820525ba35f2d" # TODO: Replace with data resource if possible
+  image_id                             = "ami-00f687b3f8812063a" # TODO: Replace with data resource if possible
   instance_type                        = "a1.large"
   key_name                             = aws_key_pair.minecraft_ec2_key.key_name
   instance_initiated_shutdown_behavior = "terminate"
@@ -155,7 +155,7 @@ resource "aws_launch_template" "minecraft_ec2_launcher" {
 resource "aws_launch_template" "minecraft_ec2_launcher_ondemand" {
   tags                                 = merge(local.tf_global_tags, local.project_name_tag)
   name                                 = "minecraft_ec2_launcher_ondemand"
-  image_id                             = "ami-0262820525ba35f2d" # TODO: Replace with data resource if possible
+  image_id                             = "ami-00f687b3f8812063a" # TODO: Replace with data resource if possible
   instance_type                        = "a1.large"
   key_name                             = aws_key_pair.minecraft_ec2_key.key_name
   instance_initiated_shutdown_behavior = "terminate"
